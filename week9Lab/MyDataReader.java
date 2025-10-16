21aa3642f378e077474163abfc0fec15896de820
crispychicken20 <cperez29@student.ohlone.edu>
2025-10-16 11:54:21 -0700

    Finished product of identifying numbers in lines of data in text file

--- week9Lab/MyDataReader.java
+++ week9Lab/MyDataReader.java
@@ -23,31 +23,57 @@ public class MyDataReader {
         int countRead = 0;
 
         try {
-            FileReader fr = new FileReader(filePath);
-            BufferedReader br = new BufferedReader(fr);
-
-            String line;
-            boolean done = false;
-            while (!done) {
-                line = br.readLine();
-                if(line == null) {
-                    done = true;
-                }
-                else {
-                    dataList.add(line);
-                    countRead++;
-
+            try (FileReader fr = new FileReader(filePath); BufferedReader br = new BufferedReader(fr)) {
+                
+                String line;
+                boolean done = false;
+                while (!done) {
+                    line = br.readLine();
+                    if(line == null) {
+                        done = true;
+                    }
+                    else {
+                        dataList.add(line);
+                        countRead++;
+                        
+                    }
                 }
             }
         } catch (IOException e) {
             System.out.println("Error in reading file: " + e.getMessage());
             return;
+
         }
+
+        
 
         System.out.println("Total Lines Read: " + countRead);
         System.out.println("--- Row Data ---");
         for(String l : dataList){
             System.out.println(l);
+        }
+
+       
+        for(int i = 0; i < dataList.size(); i++) {
+            String lines = dataList.get(i);
+            
+            int numberCount = 0;
+
+            boolean withinNumber = false;
+            
+            for(char c : lines.toCharArray()) {
+                if(Character.isDigit(c)) {
+                    if (!withinNumber) {
+                        ++numberCount;
+                        withinNumber = true;
+                    }
+                } else {
+                    withinNumber = false;
+                }
+            } 
+            if (numberCount > 0) {
+                System.out.println("line: " + (i + 1) + " has " + numberCount + " number(s)");
+            }
         }
 
 
