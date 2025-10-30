9b6077c2b28c85352029181e71fed03e66e3f6e7
crispychicken20 <cperez29@student.ohlone.edu>
2025-10-30 09:21:02 -0700

    INTIAL START TO CODEX PART II

--- PokemonCodex/MyApp.java
+++ PokemonCodex/MyApp.java
@@ -3,12 +3,36 @@ package PokemonCodex;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * class meant to read input for reading a file
+ * menu options fo user to open a file or exit
+ * print first and last of a file
+ * @author Christopher Perez
+ * @version 1.0
+ */
 public class MyApp {
+    /**
+     * Contain data within a arraylist that will be used
+     * to hold elements within a file
+     */
     private static final ArrayList<String> dataContain = new ArrayList<>(); 
+
+    /**
+     * Will be used to keep track of number of lines being
+     * read within a file
+     */
     private static int countRead = 1;
+
+
+    /**
+     * 
+     * Displayes menu to the user and returns input from user
+     * 
+     * @return userInput
+     */
     public  static int showMenu() {
         Scanner scnr = new Scanner(System.in);
 
@@ -24,6 +48,13 @@ public class MyApp {
         return userInput;
     }
 
+    /**
+     * 
+     * Reads in file from pokemon.csv
+     * adds data into arrayList of type string
+     * tracks number of lines that were read in the file
+     * @see pokemon.csv
+     */
     public static void openDataFile() {
         
 
@@ -58,7 +89,7 @@ public class MyApp {
     
                     }   
                             
-                } catch (Exception e) {
+                } catch (IOException e) {
                     System.out.println("File was not read.." + e.getMessage());
                         return;
                     }
@@ -69,12 +100,18 @@ public class MyApp {
         }
 
     }
-        
+    
+    /**
+     * Gathers first 7 lines of pokemon file
+     * 
+     * Gathers last 7 lines of pokemon file
+     */
     public static void test() {
 
         System.out.println("\n ---- first 7 lines ---");
         for(int i = 0; i < 7; i++) {
             System.out.printf("%4d: %s%n", i + 1, dataContain.get(i));
+            System.out.print(" ");
 
         }
 
@@ -83,27 +120,36 @@ public class MyApp {
         int lastSev = Math.max(1 - 1, countRead - 8);
         for(int i = lastSev; i < dataContain.size(); i++) {
             System.out.printf("%4d: %s%n", i + 1, dataContain.get(i));
+            System.out.print(" ");
         }
         System.out.println();
 
     }
 
+    /**
+     * methods called within main 
+     * menuOptions variable created for to gather user input from 
+     * opendatafile method
+     * @param args
+     */
     public static void main(String[] args) {
         int menuOptions = showMenu();
+        final int EXIT_PROGRAM = 1;
+        final int OPEN_FILE = 2;
 
-        boolean correct = false;
 
-        if(menuOptions == 1) {
-            System.out.println("Exiting... Thanks for using this application");
-        }
-        else if(menuOptions == 2) {
-            Scanner scnr = new Scanner(System.in);
-            openDataFile();
-            test();
-            
-        }
-        else {
-            System.out.println("Error: must enter a number of either 1 or 2");
+        switch (menuOptions) {
+            case EXIT_PROGRAM:
+                System.out.println("Exiting... Thanks for using this application");
+                break;
+            case OPEN_FILE:
+                Scanner scnr = new Scanner(System.in);
+                openDataFile();
+                test();
+                break;
+            default:
+                System.out.println("Error: must enter a number of either 1 or 2");
+                break;
         }
     }
 }
